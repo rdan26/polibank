6e52480d5a8e111bca3c79f4f6cbdefbaab5b93c
Alexandra <a.olalla.llamuca@gmail.com>
2026-07-12 12:21:22 -0500

    feat: Creacion e Implementacion de validacion.c y validacion.h

---
+++ src/validaciones.h
@@ -0,0 +1,12 @@
+#ifndef VALIDACIONES_H
+#define VALIDACIONES_H
+
+#define MAX_BUFFER_ENTRADA 50
+
+// Declaración de funciones de validación
+void liampiarBuffer(void);
+void leerCadenaSegura(char* destino, int longitud_max);
+float leerMontoValido(void);
+
+
+#endif // VALIDACIONES_H
